fa87c78470e13c53c8aeaabb4fba50c620923f40
Luciano <junior.romano2001@gmail.com>
2022-08-24 17:49:15 -0300

    Adicionar arquivos de projeto.

---
+++ PobAlunosFuncionarios/Pessoas.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PobAlunosFuncionarios
+{
+    internal class Pessoas
+    {
+        String Nome;
+        DateTime DataNasc;
+        long Cpf;
+    }
+   
+
+}
